vE5li/anime-foot

commit 5f83278afd0530c323d4192e1095b3d1dea644c9

config: add alpha_mode option

render.c
@@ -788,6 +788,27 @@ render_cell(struct terminal *term, pixman_image_t *pix,
                 alpha = term->colors.alpha;
             }
         }
+
+        if (!term->window->is_fullscreen) {
+            switch (term->conf->alpha_mode) {
+                case ALPHA_MODE_DEFAULT: {
+                    if (cell->attrs.bg_src == COLOR_DEFAULT) {
+                        alpha = term->colors.alpha;
+                    }
+                    break;
+                }
+                case ALPHA_MODE_MATCHING: {
+                    if (cell->attrs.bg == term->colors.bg) {
+                        alpha = term->colors.alpha;
+                    }
+                    break;
+                }
+                case ALPHA_MODE_ALL: {
+                    alpha = term->colors.alpha;
+                    break;
+                }
+            }
+        }
     }
 
     if (unlikely(is_selected && _fg == _bg)) {

config.c
@@ -1095,6 +1095,15 @@ parse_section_main(struct context *ctx)
         return true;
     }
 
+    else if (strcmp(key, "alpha-mode") == 0) {
+        _Static_assert(sizeof(conf->alpha_mode) == sizeof(int),
+        "enum is not 32-bit");
+        return value_to_enum(
+            ctx,
+            (const char *[]){"default", "matching", "all", NULL},
+            (int *)&conf->alpha_mode);
+    }
+
     else {
         LOG_CONTEXTUAL_ERR("not a valid option: %s", key);
         return false;
@@ -3338,6 +3347,7 @@ config_load(struct config *conf, const char *conf_path,
             },
             .multiplier = 3.,
         },
+        .alpha_mode = ALPHA_MODE_DEFAULT,
         .colors = {
             .fg = default_foreground,
             .bg = default_background,

config.h
@@ -167,6 +167,8 @@ struct config {
 
     enum { STARTUP_WINDOWED, STARTUP_MAXIMIZED, STARTUP_FULLSCREEN } startup_mode;
 
+    enum { ALPHA_MODE_DEFAULT, ALPHA_MODE_MATCHING, ALPHA_MODE_ALL } alpha_mode;
+
     bool dpi_aware;
     enum {GAMMA_CORRECT_DISABLED,
           GAMMA_CORRECT_ENABLED,

foot.ini
@@ -38,6 +38,8 @@
 # utmp-helper=/usr/lib/utempter/utempter  # When utmp backend is ‘libutempter’ (Linux)
 # utmp-helper=/usr/libexec/ulog-helper    # When utmp backend is ‘ulog’ (FreeBSD)
 
+# alpha-mode=default # Can be `default`, `matching` or `all`
+
 [environment]
 # name=value
 

commit cb1b7ba0c5752eeb92c72a80640fbd1f09ad4b7d

render: regression: alpha applied to inversed text/selections
Introduced by 5f83278afd0530c323d4192e1095b3d1dea644c9

Closes #2073

render.c
@@ -744,7 +744,7 @@ render_cell(struct terminal *term, pixman_image_t *pix,
             _bg = swap;
         }
 
-        if (!term->window->is_fullscreen && term->colors.alpha != 0xffff) {
+        else if (!term->window->is_fullscreen && term->colors.alpha != 0xffff) {
             switch (term->conf->colors.alpha_mode) {
             case ALPHA_MODE_DEFAULT: {
                 if (cell->attrs.bg_src == COLOR_DEFAULT) {